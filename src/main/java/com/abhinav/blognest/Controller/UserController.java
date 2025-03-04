e2e078b64bcdaed191ee4742c38c28a85729c8fa
Abhinav Tiwari <abhinavtiwari3056@gmail.com>
2025-03-04 22:07:50 +0530

    Updated UserController

--- src/main/java/com/abhinav/blognest/Controller/UserController.java
+++ src/main/java/com/abhinav/blognest/Controller/UserController.java
@@ -1,16 +1,31 @@
 package com.abhinav.blognest.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.abhinav.blognest.Repositories.UserRepo;
+import com.abhinav.blognest.DTO.UserDto;
+import com.abhinav.blognest.Services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+	
 	@Autowired
-	private UserRepo userRepo;
+	private UserService userService;
+	
+	//create
+		@PostMapping
+		public ResponseEntity<UserDto> createUser(@RequestBody  UserDto userDto)
+		{ 
+			UserDto created = this.userService.createUser(userDto);
+		    return new ResponseEntity<>(created,HttpStatus.CREATED);
+			
+		}
 	
 }
