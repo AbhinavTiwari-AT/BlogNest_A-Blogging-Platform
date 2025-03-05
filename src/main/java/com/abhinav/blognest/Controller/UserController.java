8eae7c1c2bfaa7054f8cccba1e0f6719d8e244ea
Abhinav Tiwari <abhinavtiwari3056@gmail.com>
2025-03-05 21:13:50 +0530

    Updated User

--- src/main/java/com/abhinav/blognest/Controller/UserController.java
+++ src/main/java/com/abhinav/blognest/Controller/UserController.java
@@ -1,14 +1,20 @@
 package com.abhinav.blognest.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.abhinav.blognest.DTO.UserDto;
+import com.abhinav.blognest.Entity.User;
 import com.abhinav.blognest.Services.UserService;
 
 @RestController
@@ -28,4 +34,38 @@ public class UserController {
 			
 		}
 	
+		// put - update user
+		@PutMapping("/{userId}")
+		public ResponseEntity<User> updateUser(@RequestBody UserDto userDto,@PathVariable("userId")  Integer userId)
+		{
+			User updatedUser = this.userService.updateUser(userDto, userId);
+			
+			return ResponseEntity.ok(updatedUser);
+		}
+		
+		//Delete - delete user
+		@DeleteMapping("/{userId}")
+		public String deleteUser(@PathVariable("userId")Integer userId )
+		{
+			this.userService.deleteUser()
+			return "deleted";
+		}
+		
+		//Get - user get
+		
+		@GetMapping("/")
+		
+		public ResponseEntity<List<UserDto>> getAllUser()
+		{
+			return ResponseEntity.ok(this.userService.getAllUser());
+		}
+		
+
+		@GetMapping("/{userId}")
+		
+		public ResponseEntity<String> getSingleUser(@PathVariable Integer userId)
+		{
+			return ResponseEntity.ok(this.userService.getUserById(userId));
+		}
+		
 }
